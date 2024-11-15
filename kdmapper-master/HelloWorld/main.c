c87ef5c4eff1ef374ac08c055d9ce0fe0e01cc15
dcharnic <dcharnic@gmu.edu>
2024-11-14 21:12:20 -0500

    Update main.c

--- kdmapper-master/HelloWorld/main.c
+++ kdmapper-master/HelloWorld/main.c
@@ -1,8 +1,15 @@
-
+// Include our references.
 #include <ntddk.h>
-//#include <ntdef.h>
-//s#include <ntifs.h>
 
+// This defines the no-doc MemCopy function in NTAPI
+// SourceProcess is the process that is calling the function
+// SourceAddress is the address we want to copy from
+// TargetAddress is the address we are going to place our copied memory into
+// BufferSize is the size of memory to copy
+// PreviousMode is the mode we are calling from, we are calling from kernel Here. 
+// ReturnSize is the size of the bytes that were successfully copied. If this doesn't match BufferSize, something broke.
+
+// MmCopyVirtualMemory can also be used to read memory addresses. See if you can create a new function that reads memory at an address. Use KeWriteProcessMemory as a template. It shouldn't be that different. 
 NTSTATUS NTAPI MmCopyVirtualMemory(
 	PEPROCESS SourceProcess,
 	PVOID SourceAddress,
@@ -12,12 +19,18 @@ NTSTATUS NTAPI MmCopyVirtualMemory(
 	KPROCESSOR_MODE PreviousMode,
 	PSIZE_T ReturnSize
 );
+
+// Just defining another function
+// This one places a PEPROCESS* into the the value that is passed in as Process when we give the function a PID.
 NTKERNELAPI
 NTSTATUS
 PsLookupProcessByProcessId(
 	_In_ HANDLE ProcessId,
 	_Outptr_ PEPROCESS* Process
 );
+
+// This is a simplified version of our no-doc MmCopyVirtualMemory function. 
+// Some things can be inferred / hardcoded so we only ask for what we actually need. 
 NTSTATUS KeWriteProcessMemory(
 	PEPROCESS Process,
 	PVOID SourceAddress,
@@ -42,7 +55,7 @@ NTSTATUS KeWriteProcessMemory(
 	else
 		return STATUS_ACCESS_DENIED;
 }
-
+// This is the code that gets run when the driver gets loaded, like Main()
 NTSTATUS CustomDriverEntry(
 	_In_ PDRIVER_OBJECT  kdmapperParam1,
 	_In_ PUNICODE_STRING kdmapperParam2
@@ -59,6 +72,7 @@ NTSTATUS CustomDriverEntry(
 	KdPrint(("Hello world!"));
 	int var2 = 5;
 	if (var2 == 3) {
+		// Prints the actual physical memory address of var2
 		KdPrint(("Address of var: %p\n", &var2));
 	}
 	if (var2 == 4) {
@@ -67,13 +81,15 @@ NTSTATUS CustomDriverEntry(
 	}
 	if (var2 == 5) {
 		KdPrint(("I'm about to hack this dude\n"));
-		int Writeval = 666;
-
+		// Value that will get written into the program we attack
+		int Writeval = 0xACDC;
+		// Create a new PEPROCESS type to store the pointer to the process we are attacking
 		PEPROCESS Process;
 		PsLookupProcessByProcessId(13016, &Process); //lookup the process by it's id;
-
+		// Write "Writeval" to the process in "Process" at the memory address pointed to by the 3rd argument. We are going to be writing a 32 bit int, so our buffer must be that big.
 		KeWriteProcessMemory(Process, &Writeval, 0x8BBE35F9E0, sizeof(__int32));
 
+		// If everything worked, the program will print out the new value. 
 		DbgPrint("Value of int i: %d", Writeval);
 
 		return STATUS_SUCCESS;
